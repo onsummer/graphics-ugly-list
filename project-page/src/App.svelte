d332a88902362940b17756ebcf7fcc0ac32370ae
onsummer <onsummer@foxmail.com>
2021-05-28 11:42:03 +0800

    [add] font support.

--- project-page/src/App.svelte
+++ project-page/src/App.svelte
@@ -11,24 +11,41 @@
 </div>
 <div id="root">
   {#each companyData as { fullname, enName, where }}
-    <InfoCard title={fullname} contents={[`英文名：${enName}`, `所在地：${where}`]}/>
+    <InfoCard
+      title={fullname}
+      contents={[`英文名：${enName}`, `所在地：${where}`]}
+    />
   {/each}
 </div>
 
 <style>
+  @font-face {
+    font-family: FiraCode;
+    src: url("/graphics-ugly-list/font/FiraCode-Regular.woff2"),
+      url("/graphics-ugly-list/font/FiraCode-Regular.woff");
+  }
+
+  @font-face {
+    font-family: Helvetica;
+    src: url("/graphics-ugly-list/font/Helvetica.woff");
+  }
+
   #root {
+    font-family: "Helvetica";
     display: flex;
     flex-direction: row;
   }
 
   .code {
+    font-family: "FiraCode";
     border: 1px solid rgb(241, 241, 241);
-    border-radius: .8rem;
+    border-radius: 0.8rem;
     padding: 0.5rem;
     background-color: rgb(230, 230, 230);
   }
 
   .title {
+    letter-spacing: 1px;
     font-size: 2rem;
     margin: 2rem 0;
   }
